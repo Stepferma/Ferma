74240529026d46fafeaea7c68e13498a339e456a
ЗатовД <@DESKTOP-9F85AP5>
2019-02-27 20:28:52 +0600

    Add migration (Add_class_Farms) Add migration (Add_class_Stocks)

---
+++ Ferma.DAL/Entities/Farms.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferma.DAL.Entities
+{
+    public class Farms
+    {
+        [Key]
+        public int? IdFarm { get; set; }
+        public int? IdPlayer { get; set; }
+        public int? IdStock { get; set; }
+    }
+}
---
+++ Ferma.DAL/Entities/Stocks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferma.DAL.Entities
+{
+    public class Stocks
+    {
+        [Key]
+        public int? IdStock { get; set; }
+        public int? Grass { get; set; }
+        public int? Seed { get; set; }
+        public int? Pork { get; set; }
+        public int? Eggs { get; set; }
+    }
+}
