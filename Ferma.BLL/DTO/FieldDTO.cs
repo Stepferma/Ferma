90949df69f518d721704efd9dbeb36cbb74a342e
Daryn <user@DESKTOP-ABVHRN0>
2019-03-04 15:25:22 +0600

    add services FarmDTO,PlayerDTO,PlayerDTO

---
+++ Ferma.BLL/DTO/FieldDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace Ferma.BLL.DTO
+{
+    public class FieldDTO
+    {
+        public int IdField { get; set; }
+        public int IdBuilding { get; set; }
+        public int IdFarm { get; set; }
+    }
+}
---
+++ Ferma.BLL/Services/FieldService.cs
@@ -0,0 +1,24 @@
+﻿using Ferma.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Ferma.BLL.Services
+{
+    public class FieldService : IService<UserDTO>
+    {
+        public FieldService()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello Page" }
+                }
+            };
+        }
+    }
+}
