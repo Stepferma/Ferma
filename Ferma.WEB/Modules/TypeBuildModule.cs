ec014e759949b92ec88dd6bc18684c223ae057a2
Александр <cenitelas@mail.ru>
2019-03-06 19:16:51 +0600

    Add Module for TypeProduct and TypeBuildings

---
+++ Ferma.WEB/Modules/TypeBuildModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ferma.BLL.DTO;
+using Ferma.BLL.Interfaces;
+using Ferma.BLL.Services;
+using Ninject.Modules;
+
+namespace Ferma.Modules
+{
+    public class TypeBuildModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IService<TypeBuildingsDTO>>().To<TypeBuildingService>();
+        }
+    }
+}
---
+++ Ferma.WEB/Modules/TypeProductModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ferma.BLL.DTO;
+using Ferma.BLL.Interfaces;
+using Ferma.BLL.Services;
+using Ninject.Modules;
+
+namespace Ferma.Modules
+{
+    public class TypeProductModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IService<TypeProductsDTO>>().To<TypeProductsService>();
+        }
+    }
+}
